5ed3077d27229c5add1d923f0c421610a3991fa8
ssdfffffffffffff <d15828974539@outlook.com>
2026-09-12 20:24:39 +0800

    Add files via upload

---
+++ 新建 Python 源文件.py
@@ -0,0 +1,17 @@
+import turtle as a
+a.setup(1000,1000)
+a.bgcolor("white")
+a.pencolor("black")
+a.title("hi world")
+a.forward(100)
+A = a.penup()
+A
+B = a.pendown()
+B
+a.circle(50)
+a.setup(10,10)
+for i in range(4):
+    a.left(90)
+    a.forward(100)
+    a.setup(1000,1000)
+a.done()
